1360277a5d4ed1bbf4c8ff15cc274cfe35b62709
eduramza <eduardo.rmb@outlook.com>
2024-01-29 00:55:57 -0300

    :dizzy: add splash screen for light and dark mode

--- app/src/main/java/com/eduramza/personaldiaryapp/MainActivity.kt
+++ app/src/main/java/com/eduramza/personaldiaryapp/MainActivity.kt
@@ -3,14 +3,15 @@ package com.eduramza.personaldiaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.eduramza.personaldiaryapp.ui.theme.PersonalDiaryAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             PersonalDiaryAppTheme {
-
             }
         }
     }
