3aaf3f1733528b020ca68541fd9397b8aa31b112
eduramza <eduardo.rmb@outlook.com>
2024-01-29 09:44:50 -0300

    :closed_lock_with_key: set ids to mongo and google clients

---
+++ app/src/main/java/com/eduramza/personaldiaryapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.eduramza.personaldiaryapp.util
+
+object Constants {
+    const val MONGO_SERVICE_APP_ID = "application-0-eyrrk"
+    const val GOOGLE_CLIENT_ID = "658712362415-nandg71ev1498aijopo09192ud9n9j5k.apps.googleusercontent.com"
+}
